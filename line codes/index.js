f57a603213db22734f1dd3283f33755a3b80214b
Mustapha ben khelifa <benkhelifaoussema01@gmail.com>
2024-01-11 17:02:55 +0100

    search

---
+++ line codes/index.js
@@ -0,0 +1,12 @@
+// factory function to upload cources to our plateform
+function generateCourses(name,src,category){
+    return {
+        name:name,
+        src:src,
+        category:category
+    }
+}
+// add is a function wich allow us to add courses to our platform
+function add(){
+
+}
