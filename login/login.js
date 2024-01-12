7b4fc8eb01298ab6cf2062b939fe65029d5920ee
nextwarrior000 <yassinesalem543@gmail.com>
2024-01-12 16:10:39 +0100

    neww

--- login/login.js
+++ login/login.js
@@ -1,29 +1,11 @@
-function registration(){
-var firstname=document.getElementById('#FirstName').value
-var lastname=document.getElementById('#Lastnme').value
-var email=document.getElementById('#email').value
-var password=document.getElementById('#Password').value
-var verifyPassword=document.getElementById('#verifyPssword').value
-// for testing
-        var password_expression = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
-		var letters = /^[A-Za-z]+$/;
-		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if(firstname===''){
-        alert('please enter your FirstName !')
-    }
-    else if(lastname===''){
-        alert('please enter you LastName !')
-    }
-    
+$(document).ready(function () {
+  $("#register-link").click(function () {
+    window.location.href = "./signin.html";
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-}
+$(document).ready(function () {
+  $(".smooth-button").click(function () {
+    window.location.href = "./service.html";
+  });
+});
