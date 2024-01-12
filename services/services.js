cde738a30bf404366fcc85859991b5c0571fd37d
Mustapha ben khelifa <benkhelifaoussema01@gmail.com>
2024-01-12 06:58:10 +0100

    services login localStorage

---
+++ services/services.js
@@ -0,0 +1,20 @@
+function CoursesMaker(name,src,category){
+    var cources={}
+    courses.name=name
+    cources.src=src
+    cources.category=category
+    cources.CourceList=[]
+    cources.addCource=addCource
+    cources.displayCource=displayCource
+    cources.removeCource=removeCource
+    return cources
+}
+function addCource(){
+
+}
+function displayCource(){
+
+}
+function removeCource(){
+
+}
